77648859e5cafc7a50f5d797dbdbde5fcbb62a94
Yevhenii <stupachenko30@gmail.com>
2026-06-11 10:26:23 +0200

    add

--- src/main/java/MainGarage.java
+++ src/main/java/MainGarage.java
@@ -3,3 +3,31 @@ public class MainGarage {
 
     }
 }
+/*Car
+   -----
+model(String)
+year(int)
+regNumber(String)
+code(String)
+serviceType(String) -> not in constructor
+
+        Owner
+   --------
+name(String)
+phone(String)
+id(int)
+address(String)
+
+Garage
+  ----------
+name(String)
+address(String)
+phone(String)
+TreeMap<Car,Owner> cars
+  -----------------------
+public void addCar(Car c,Owner ow) or(или)
+public void addCar(Car c,Owner ow,String service)
+public void printCarsInProcess()
+public void returnCarToOwner(String regNumber)
+
+ */
